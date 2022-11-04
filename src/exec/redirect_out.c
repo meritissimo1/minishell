a6a951ad89ed9b9ae9551cdb8d3ddaac85908a9c
meritissimo1 <marcelomoreira1905@gmail.com>
2022-11-04 13:09:35 -0400

    debbuging redirect

--- src/exec/redirect_out.c
+++ src/exec/redirect_out.c
@@ -40,4 +40,76 @@ void	simple_redir_out(t_minishell *mini, int i, int flags)
 
 	aux = ft_strtrim(&mini->commands[i][1], " ");
 	file = ft_substr
-}*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void	redirect_out(t_struct *mini, int j)
+{
+	int		flags;
+	char	*file;
+
+	flags = O_WRONLY | O_CREAT;
+	j = mini->c;
+	if (mini->commands[j] && mini->commands[j][0] == '>')
+	{
+		if (mini->commands[j] && mini->commands[j][1] == '>')
+		{
+			file = ft_strtrim(&mini->commands[j][2], " ");
+			mini->out_fd = open(file, flags | O_APPEND, 0777);
+			free (file);
+		}
+		else
+			simple_redir_out(mini, j, flags);
+		mini->last_redir = 1;
+		if (mini->split.n_comand == 1)
+			free(mini->line);
+	}
+}
+
+void	simple_redir_out(t_struct *mini, int j, int flags)
+{
+	char	*aux;
+	char	*file;
+
+	aux = ft_strtrim(&mini->commands[j][1], " ");
+	file = ft_substr(aux, 0, find_char(aux, ' '));
+	mini->out_fd = open(file, flags | O_TRUNC, 0777);
+	free (aux);
+	free (file);
+}
+
+int	find_char(char *string, char needle)
+{
+	int	i;
+
+	i = 0;
+	while (string[i])
+	{
+		if (string[i] == needle)
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+*/
