19b35f0a8906c82eb21083f3573757a5fecc4fbc
Marcelo | marcrodr <marcelomoreira1905@gmail.com>
2022-12-02 19:26:09 -0400

    changed file permissions to 644

--- src/exec/redirect_out.c
+++ src/exec/redirect_out.c
@@ -6,7 +6,7 @@
 /*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 09:55:03 by marcrodr          #+#    #+#             */
-/*   Updated: 2022/11/01 09:15:10 by marcrodr         ###   ########.fr       */
+/*   Updated: 2022/12/02 19:11:17 by marcrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	redirect_out(t_minishell *mini, int i)
 		if (mini->commands[i] && mini->commands[i][1] == '>')
 		{
 			file = ft_strtrim(&mini->commands[i][2], " ");
-			mini->out_fd = open(file, flags | O_APPEND, 0777);
+			mini->out_fd = open(file, flags | O_APPEND, 0644);
 			printf("well well whats here fd:%d\nfile: %s\n", mini->out_fd, file);
 			free(file);
 		}
@@ -40,7 +40,7 @@ void	simple_redir_out(t_minishell *mini, int i, int flags)
 
 	aux = ft_strtrim(&mini->commands[i][1], " ");
 	file = ft_substr(aux, 0, find_char(aux, ' '));
-	mini->out_fd = open(file, flags | O_TRUNC, 0777);	
+	mini->out_fd = open(file, flags | O_TRUNC, 0644);	
 	free(aux);
 	free(file);
 }
@@ -59,56 +59,3 @@ int	find_char(char *str, char wanted)
 	}
 	return (i);
 }
-
-
-
-/*
-void	redirect_out(t_struct *mini, int j)
-{
-	int		flags;
-	char	*file;
-
-	flags = O_WRONLY | O_CREAT;
-	j = mini->c;
-	if (mini->commands[j] && mini->commands[j][0] == '>')
-	{
-		if (mini->commands[j] && mini->commands[j][1] == '>')
-		{
-			file = ft_strtrim(&mini->commands[j][2], " ");
-			mini->out_fd = open(file, flags | O_APPEND, 0777);
-			free (file);
-		}
-		else
-			simple_redir_out(mini, j, flags);
-		mini->last_redir = 1;
-		if (mini->split.n_comand == 1)
-			free(mini->line);
-	}
-}
-
-void	simple_redir_out(t_struct *mini, int j, int flags)
-{
-	char	*aux;
-	char	*file;
-
-	aux = ft_strtrim(&mini->commands[j][1], " ");
-	file = ft_substr(aux, 0, find_char(aux, ' '));
-	mini->out_fd = open(file, flags | O_TRUNC, 0777);
-	free (aux);
-	free (file);
-}
-
-int	find_char(char *string, char needle)
-{
-	int	i;
-
-	i = 0;
-	while (string[i])
-	{
-		if (string[i] == needle)
-			return (i);
-		i++;
-	}
-	return (i);
-}
-*/
